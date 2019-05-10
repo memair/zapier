6f3e38d977084f920725b20424329adea60dba39
gregology <greg@gho.st>
2019-05-10 19:26:55 -0400

    fixing test

--- test/basic.js
+++ test/basic.js
@@ -9,8 +9,8 @@ describe('oauth2 app', () => {
   before(() => {
     // It's a good idea to store your Client ID and Secret in the environment rather than in code.
     // This works locally via the `export` shell command and in production by using `zapier env`
-    if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
-      throw new Error('For the tests to run, you need to do `export CLIENT_ID=1234 CLIENT_SECRET=asdf`');
+    if (!process.env.CLIENT_ID) {
+      throw new Error('For the tests to run, you need to do `export CLIENT_ID=1234`');
     }
   });
 
